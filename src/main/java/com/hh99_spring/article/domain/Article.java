7d6d58f71155a47dcc70532c6126b6e249b8aee5
bebest90 <1990devops@gmail.com>
2021-07-15 12:48:34 +0900

    Initial commit

---
+++ src/main/java/com/hh99_spring/article/domain/Article.java
@@ -0,0 +1,40 @@
+package com.hh99_spring.article.domain;
+
+import com.hh99_spring.article.dto.AritcleRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Setter
+public class Article extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String contents;
+
+    public Article(AritcleRequestDto aritcleRequestDto){
+        this.username = aritcleRequestDto.getUsername();
+        this.title = aritcleRequestDto.getTitle();
+        this.contents = aritcleRequestDto.getContents();
+    }
+
+    public void update(AritcleRequestDto aritcleRequestDto){
+        this.username = aritcleRequestDto.getUsername();
+        this.title = aritcleRequestDto.getTitle();
+        this.contents = aritcleRequestDto.getContents();
+    }
+}
